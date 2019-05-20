9437c1595d3b81ed652d2579ef408ca30fbb587e
Common Training user <CT0t1868@TechMahindra.com>
2019-05-20 16:55:46 +0530

    fisrt spring boot application with controller

---
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -0,0 +1,17 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/")
+    public String sayHello() {
+        return "hello";
+    }
+
+    @GetMapping("/sri")
+    public String sayHelloSri() {
+        return "hello sridhar";
+    }
+}
