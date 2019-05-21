7bb61c54941efcaca61d465dc93a412dd2aa12ab
Common Training user <CT0t1868@TechMahindra.com>
2019-05-21 11:36:40 +0530

    configuring an app lab

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,17 +1,22 @@
 package io.pivotal.pal.tracker;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
-    @GetMapping("/")
-    public String sayHello() {
-        return "hello";
+
+    private String helloMessage;
+
+    public WelcomeController( @Value("${WELCOME_MESSAGE}") String helloMessage) {
+        this.helloMessage = helloMessage;
     }
 
-    @GetMapping("/sri")
-    public String sayHelloSri() {
-        return "hello sridhar";
+    @GetMapping("/")
+    public String sayHello() {
+        return helloMessage;
     }
-}
+
+    }
